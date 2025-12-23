d4f07a7a7740fa545c1ce829bf7475b511861f36
syafaatsaat <47024629+syafaatsaat@users.noreply.github.com>
2025-12-23 16:57:45 +0800

    Make calculator work (1 operator at a time)

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    if (num2 === 0) {
+        return "Undefined";
+    }
+
     return num1 / num2;
 }
 
@@ -20,15 +24,49 @@ function operate(operator, num1, num2) {
             return add(num1, num2);
         case '-':
             return subtract(num1, num2);
-        case '*':
+        case '×':
             return multiply(num1, num2);
-        case '/':
+        case '÷':
             return divide(num1, num2);
     }
 }
 
 const displayText = document.querySelector("#display");
 const buttons = document.querySelector("#buttons");
+
+function checkOperatorUsed() {
+    const operators = ['+', '-', '×', '÷'];
+    let currentExpression = displayText.textContent;
+    for (const operator of operators) {
+        if (currentExpression.includes(operator)) {
+            return {
+                result: true,
+                operator: operator,
+            };
+        }
+    }
+    return {
+        result: false
+    };
+}
+
+function insertOperator(operator) {
+    const operators = ['+', '-', '×', '÷']; 
+    let currentExpression = displayText.textContent;
+    if (checkOperatorUsed().result) 
+    {
+        let displayTextLen = currentExpression.length;
+        let lastVariableUsed = currentExpression[displayTextLen - 1];
+        if (operators.includes(lastVariableUsed)) {
+            removeVariable();
+        }
+        else {
+            return;
+        }
+    }
+
+    displayText.textContent += operator;
+}
 
 function insertDigit(digit) {
     if (displayText.textContent === "0") {
@@ -41,14 +79,32 @@ function insertDigit(digit) {
     }
 }
 
-function removeDigit() {
-    let numbers = displayText.textContent;
-    if (numbers.length < 2) {
+function removeVariable() {
+    let expression = displayText.textContent;
+    if (expression.length < 2) {
         displayText.textContent = "0";
     }
     else {
-        numbers = numbers.slice(0, numbers.length - 1);
-        displayText.textContent = numbers;
+        newExpression = expression.slice(0, expression.length - 1);
+        displayText.textContent = newExpression;
+    }
+}
+
+function computeResult() {
+    const operatorUsed = checkOperatorUsed();
+    let result = "";
+    if (operatorUsed.result) {
+        let variables = displayText.textContent.split(operatorUsed.operator);
+        if (variables[1] === "") {
+            result = variables[0];
+        }
+        else {
+            let num1 = +variables[0];
+            let num2 = +variables[1];
+            result = operate(operatorUsed.operator, num1, num2);
+        }
+
+        displayText.textContent = result;
     }
 }
 
@@ -68,8 +124,23 @@ buttons.addEventListener('click', (event) => {
         case '0':
             insertDigit(target.id);
             break;
+        case 'add':
+            insertOperator('+');
+            break;
+        case 'subtract':
+            insertOperator('-');
+            break;
+        case 'multiply':
+            insertOperator('×');
+            break;
+        case 'divide':
+            insertOperator('÷');
+            break;
+        case 'equal':
+            computeResult();
+            break;
         case 'backspace':
-            removeDigit();
+            removeVariable();
             break;
         case 'clear':
             displayText.textContent = "0";
